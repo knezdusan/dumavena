0945d2661c6d8ce32550b9859bc601ba27a5899c
Dusan Knezevic <knez.dusan@gmail.com>
2023-06-28 15:30:54 +0200

    final push2

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,12 @@
 	@vite('resources/css/app.css')
 	@vite('resources/js/app.js')
 
+	<link rel="icon" href="{{ asset('favicons/favicon.ico') }}" type="image/x-icon"/>
+	<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-touch-icon.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+	<link rel="manifest" href="{{ asset('favicons/site.webmanifest') }}">
+
 </head>
 <body {{ $attributes->merge(['class' => 'text-gray-300 bg-gray-900']) }}>
 	@if ($errors->any())
